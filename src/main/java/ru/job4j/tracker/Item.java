330297f1259ce42912b4969aeb86098832dad199
olga.ilyina <olga.ilyina@axenix.pro>
2024-10-05 22:58:52 +0300

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,14 +1,22 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
+import static java.time.LocalDateTime.now;
+
 public class Item {
     private int id;
     private String name;
 
+    private LocalDateTime created;
+
     public Item() {
+        created = now();
     }
 
     public Item(String name) {
         this.name = name;
+        created = now();
     }
 
     public Item(int id, String name) {
@@ -30,5 +38,9 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 }
