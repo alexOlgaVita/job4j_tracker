51b4e8efef4097e8c110c4829d0766950154dd91
olga.ilyina <olga.ilyina@axenix.pro>
2024-10-06 10:40:14 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,35 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book cleanArchitecture = new Book("Clean Architecture: A Craftsman's Guide to Software", 293);
+        Book functionalDesign = new Book("Functional Design: Principles, Patterns, and Practices", 317);
+        Book cleanCode = new Book("Clean code", 210);
+        Book extremeProgramming = new Book("Extreme Programming in Practice", 186);
+        Book[] books = new Book[4];
+        books[0] = cleanArchitecture;
+        books[1] = functionalDesign;
+        books[2] = cleanCode;
+        books[3] = extremeProgramming;
+        System.out.println("Список книг до перестановки:");
+        for (int index = 0; index < books.length; index++) {
+            Book book = books[index];
+            System.out.println("\"" + book.getName() + "\", количество страниц: " + book.getPages());
+        }
+        Book temp = books[0];
+        books[0] = books[3];
+        books[3] = temp;
+        System.out.println("Список книг после перестановки:");
+        for (int index = 0; index < books.length; index++) {
+            Book book = books[index];
+            System.out.println("\"" + book.getName() + "\", количество страниц: " + book.getPages());
+        }
+        System.out.println("Книги только с названием = \"Clean code\"");
+        for (int index = 0; index < books.length; index++) {
+            Book book = books[index];
+            if (book.getName() == "Clean code") {
+                System.out.println("\"" + book.getName() + "\", количество страниц: " + book.getPages());
+            }
+        }
+    }
+}
