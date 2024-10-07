bc122edc8b26543ff25e49d80f8775aa497bb462
olga.ilyina <olga.ilyina@axenix.pro>
2024-10-07 21:41:06 +0300

    5.0. Приведение типов. Повышение типа и понижение типа. [#181322]

---
+++ src/main/java/ru/job4j/oop/Freshman.java
@@ -0,0 +1,7 @@
+package ru.job4j.oop;
+
+public class Freshman extends Student {
+    public void saveStudentCard() {
+        System.out.println("Трепетно ухаживаю за студенческим билетом");
+    }
+}
