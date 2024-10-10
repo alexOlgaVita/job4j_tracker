7f74886131fd06a93b957819e71ccd53f86ad5ce
olga.ilyina <olga.ilyina@axenix.pro>
2024-10-10 22:50:26 +0300

    0.6. Error - исключения, связанные с работой виртуальной машины [#219370]

--- src/main/java/ru/job4j/ex/Factorial.java
+++ src/main/java/ru/job4j/ex/Factorial.java
@@ -1,18 +1,16 @@
 package ru.job4j.ex;
 
 public class Factorial {
-    public int calc(int number) {
-        if (number < 0) {
-            throw new IllegalArgumentException("Number could not be less than 0");
+    public static int calc(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return calc(n - 1) * n;
         }
-        int result = 1;
-        for (int index = 2; index <= number; index++) {
-            result *= index;
-        }
-        return result;
     }
 
     public static void main(String[] args) {
-        new Factorial().calc(-1);
+        int result = calc(3);
+        System.out.println(result);
     }
 }
