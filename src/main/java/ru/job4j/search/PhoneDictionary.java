9271dbd24534d99e7210fa526b0ee1eb52ca9730
olga.ilyina <olga.ilyina@axenix.pro>
2024-10-15 07:24:53 +0300

    4. Телефонный справочник на базе ArrayList [#41598 #517398]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -17,19 +17,10 @@ public class PhoneDictionary {
     public ArrayList<Person> find(String key) {
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
-            if (person.getPhone().contains(key)) {
-                result.add(person);
-                continue;
-            }
-            if (person.getName().contains(key)) {
-                result.add(person);
-                continue;
-            }
-            if (person.getAddress().contains(key)) {
-                result.add(person);
-                continue;
-            }
-            if (person.getSurname().contains(key)) {
+            if (person.getPhone().contains(key)
+                    || (person.getName().contains(key)
+                    || person.getAddress().contains(key)
+                    || person.getSurname().contains(key))) {
                 result.add(person);
             }
         }
