e0d1fef941fd9c1876ca3788f1b612d0b2aedb11
olga.ilyina <olga.ilyina@axenix.pro>
2024-10-17 07:56:57 +0300

    0. Map, HashMap [#212652 #517773]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,8 +6,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("olilijna@mail.ru", "Olga Ilyina");
-        map.put("svetakonfeta0901@gmail.com", "Olga Ilyina");
-        map.put("kolapro522@gmail.com", "Olga Ilyina");
+        map.put("svetakonfeta0901@gmail.com", "Sveta Ilyina");
+        map.put("kolapro522@gmail.com", "Nikolay Ilyina");
+        map.put("svetakonfeta0901@gmail.com", "Svetik Ilyina");
         for (String key : map.keySet()) {
             System.out.println("key = " + key + ", value = " + map.get(key));
         }
