79b4bfe33c64a176b15c9bc8939973abc2279c5c
olga.ilyina <olga.ilyina@axenix.pro>
2024-10-21 07:52:45 +0300

    3. Отсортировать департаменты [#34352 #518304]

--- src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
+++ src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -5,27 +5,9 @@ import java.util.Comparator;
 public class DepartmentsDescComparator implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        int result = 0;
         String[] leftS = left.split("/");
         String[] rightS = right.split("/");
-        for (int i = 0; i < Math.min(leftS.length, rightS.length); i++) {
-            for (int j = 0; j < Math.min(rightS[i].length() - 1, leftS[i].length() - 1); j++) {
-                result = Character.compare(rightS[i].charAt(j), leftS[i].charAt(j));
-            }
-            if (result != 0) {
-                return result;
-            }
-            result = Integer.compare(leftS[i].length(), rightS[i].length());
-            if (result != 0) {
-                return result;
-            }
-            result = (i == 0) ? Integer.compare(rightS[i].charAt(rightS[i].length() - 1), leftS[i].charAt(leftS[i].length() - 1)) : Integer.compare(leftS[i].charAt(leftS[i].length() - 1), rightS[i].charAt(rightS[i].length() - 1));
-            if (result != 0) {
-                return result;
-            }
-
-        }
-        result = Integer.compare(leftS.length, rightS.length);
-        return result;
+        int result = rightS[0].compareTo(leftS[0]);
+        return result != 0 ? result : left.compareTo(right);
     }
 }
